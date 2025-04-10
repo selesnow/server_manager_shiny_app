9249dd36039c8dc9483ddf9ff2d3b0033f217d72
Alexey Alsey Seleznev <a.seleznev@netpeak.group>
2025-04-10 12:56:27 +0200

    Доработана вкладка процессы --test
    ### Changed
    - Доработан интерфейс вкладки **Процессы**:
        - Добавлены фильтры по полям имя процесса и клиент.
        - В таблице процессов добавлены фильтры под каждое поле.
        - Все фильтры теперь взаимосвязаны, т.е. при выборе значения в одном, изменяются список доступных значений в остальных фильтрах.
    ### Fixed
    - Исправлено вычисления поля `client` в таблице **процессов**.

--- modules/mod_tab_processes.R
+++ modules/mod_tab_processes.R
@@ -12,9 +12,14 @@ mod_tab_processes_ui <- function(id) {
             div(class = "card-body",
                 fluidRow(
                   column(
-                    width = 8,
+                    width = 4,
                     selectInput(ns("filter_username"), "Пользователь", choices = NULL, multiple = TRUE),
-                    selectInput(ns("filter_dir"), "Рабочая директория", choices = NULL, multiple = TRUE, width = "100%"),
+                    selectInput(ns("filter_name"), "Имя процесса", choices = NULL, multiple = TRUE),
+                    selectInput(ns("filter_client"), "Клиент", choices = NULL, multiple = TRUE),
+                    selectInput(ns("filter_dir"), "Рабочая директория", choices = NULL, multiple = TRUE, width = "90%")
+                  ),
+                  column(
+                    width = 4,
                     selectInput(ns("filter_files"), "Открытый файл", choices = NULL, multiple = FALSE, width = "100%"),
                     actionButton(ns("kill_process"), "Остановить процессы", class = "btn-danger", style = "margin-top: 10px; width: 100%")
                   )
@@ -38,7 +43,6 @@ mod_tab_processes_ui <- function(id) {
   )
 }
 
-
 mod_tab_processes_server <- function(id, refresh_trigger) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
@@ -61,20 +65,27 @@ mod_tab_processes_server <- function(id, refresh_trigger) {
     observe({
       data <- processes()
       updateSelectInput(session, "filter_username", choices = unique(data$username))
-      updateSelectInput(session, "filter_dir", choices = unique(data$dir))
+      updateSelectInput(session, "filter_name", choices = unique(data$name))
+      updateSelectInput(session, "filter_client", choices = unique(data$client))
     })
     
-    # Динамически подгружаем список файлов, в зависимости от username и dir
     observe({
       data <- processes()
       
       if (!is.null(input$filter_username)) {
         data <- data[data$username %in% input$filter_username, ]
       }
+      if (!is.null(input$filter_name)) {
+        data <- data[data$name %in% input$filter_name, ]
+      }
+      if (!is.null(input$filter_client)) {
+        data <- data[data$client %in% input$filter_client, ]
+      }
       if (!is.null(input$filter_dir)) {
         data <- data[data$dir %in% input$filter_dir, ]
       }
       
+      updateSelectInput(session, "filter_dir", choices = unique(data$dir))
       updateSelectInput(session, "filter_files", choices = unique(data$files))
     })
     
@@ -84,26 +95,33 @@ mod_tab_processes_server <- function(id, refresh_trigger) {
       if (!is.null(input$filter_username)) {
         data <- data[data$username %in% input$filter_username, ]
       }
+      if (!is.null(input$filter_name)) {
+        data <- data[data$name %in% input$filter_name, ]
+      }
+      if (!is.null(input$filter_client)) {
+        data <- data[data$client %in% input$filter_client, ]
+      }
       if (!is.null(input$filter_dir)) {
         data <- data[data$dir %in% input$filter_dir, ]
-      }
-      if (!is.null(input$filter_files)) {
-        data <- data[data$files == input$filter_files, ]
       }
       
       data
     })
     
     output$process_table <- renderDT({
-      datatable(filtered_processes(), options = list(pageLength = 10, scrollX = TRUE))
+      datatable(filtered_processes(), filter = "top", options = list(pageLength = 25, scrollX = TRUE))
     })
     
     observeEvent(input$kill_process, {
       req(input$filter_files)
       
-      pids <- filtered_processes() %>% pull(pid) %>% unique()
+      data <- processes()
+      target_pids <- data %>% 
+        filter(files == input$filter_files) %>% 
+        pull(pid) %>% 
+        unique()
       
-      for (pid in pids) {
+      for (pid in target_pids) {
         tryCatch({
           ps::ps_kill(ps::ps_handle(pid))
         }, error = function(e) {
@@ -112,9 +130,7 @@ mod_tab_processes_server <- function(id, refresh_trigger) {
       }
       
       showNotification("Процессы остановлены", type = "message")
-      
-      processes(get_processes())  # Обновим после остановки
+      processes(get_processes())
     })
   })
-  
 }
