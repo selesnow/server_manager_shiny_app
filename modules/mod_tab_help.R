b70fe1200c06d2f933aa718c1d2eef059a3dcc02
Alexey Alsey Seleznev <a.seleznev@netpeak.group>
2025-04-19 18:56:00 +0200

    В README добавлен видео мануал --test

--- modules/mod_tab_help.R
+++ modules/mod_tab_help.R
@@ -1,49 +1,61 @@
-# UI часть модуля
 mod_help_ui <- function(id) {
   ns <- NS(id)
   
-  # Возвращаем список из двух вкладок
   tabPanel(
-      title = "README",
-      fluidRow(
-        column(
-          width = 12,
-          div(class = "card",
-              div(class = "card-body",
-                  div(class = "stats-summary",
-                      uiOutput(ns("overall_stats_summary"))
-                  ),
-                  div(class = "stats-description",
-                      h4("О приложении:"),
-                      HTML(str_glue("
+    title = "README",
+    
+    # Верхний блок со статистикой и описанием
+    fluidRow(
+      column(
+        width = 12,
+        div(class = "card",
+            div(class = "card-body",
+                div(class = "stats-summary",
+                    uiOutput(ns("overall_stats_summary"))
+                ),
+                div(class = "stats-description",
+                    h4("О приложении:"),
+                    HTML(str_glue("
                       <ul>
                         <li><b>Название:</b> {yaml::read_yaml(here::here('app_info.yml'))$name}</li>
                         <li><b>Версия:</b> {yaml::read_yaml(here::here('app_info.yml'))$version}</li>
                         <li><b>Краткое описание: </b>{yaml::read_yaml(here::here('app_info.yml'))$description}</li>
                       </ul>
-                    ")
-                   )
-                  )
-              )
-          )
-        )
-      ),
-      fluidRow(
-        column(
-          width = 12,
-            div(class = "card",
-                div(class = "card-body",
-                    includeMarkdown(here::here("README.md"))
+                    "))
                 )
+            )
+        )
       )
+    ),
+    
+    fluidRow(
+      column(
+        width = 12,
+        div(class = "card",
+            div(class = "card-body",
+                h4("Видео мануал:"),
+                tags$iframe(
+                  src = "https://www.youtube.com/embed/RBn8vfgrVUo?si=59EpzUUFcyhD0eVe",
+                  width = "560",
+                  height = "315",
+                  frameborder = "0",
+                  allowfullscreen = NA
+                )
+            )
+        )
+      )
+    ),
+    
+    # Блок с README.md
+    fluidRow(
+      column(
+        width = 12,
+        div(class = "card",
+            div(class = "card-body",
+                includeMarkdown(here::here("README.md"))
+            )
         )
       )
     )
-}
-
-# Серверная часть модуля
-mod_help_server <- function(id) {
-  moduleServer(id, function(input, output, session) {
-    # Серверная логика не требуется, так как содержимое статическое
-  })
+  )
 }
