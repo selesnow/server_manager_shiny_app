5792c8d27f7d56984842facd31558adbec2b2304
Alexey Alsey Seleznev <a.seleznev@netpeak.group>
2025-09-12 12:54:36 +0200

    На вкладку **Статистика** добавлена таблица "Количество сбоев по задачам".

--- modules/mod_tab_statistic.R
+++ modules/mod_tab_statistic.R
@@ -26,7 +26,7 @@ mod_tab_statistic_ui <- function(id) {
                 )
             )
         )
-      ),
+      )
     ),
     fluidRow(
       column(12, style = "height: 44.2vh;", plotOutput(ns("proj_elements_plot"), height = "100%"))
@@ -64,6 +64,20 @@ mod_tab_statistic_ui <- function(id) {
         )
       )
     ),
+    # >>> НОВЫЙ БЛОК С ФИЛЬТРОМ И ТАБЛИЦЕЙ <<<
+    fluidRow(
+      column(
+        width = 12,
+        div(class = "card",
+            div(class = "card-header", "Количество сбоев по задачам"),
+            div(class = "card-body",
+                dateRangeInput(ns("error_date_range"), "Дата", 
+                               start = Sys.Date() - 30, end = Sys.Date()),
+                DTOutput(ns("task_errors_table"))
+            )
+        )
+      )
+    ),
     fluidRow(
       column(6, plotOutput(ns("task_log_plot"))),
       column(6, plotOutput(ns("task_info_plot")))
@@ -71,31 +85,48 @@ mod_tab_statistic_ui <- function(id) {
   )
 }
 
-# Серверная часть модуля статистики
+# SERVER
 mod_tab_statistic_server <- function(id, all_tasks, conf_rv) {
   moduleServer(id, function(input, output, session) {
-    # Реактивные значения для статистики
+    con <- dbConnect(SQLite(), conf_rv()$database_settings$task_log_base)
+    
+    # ---- Реактивы для существующей статистики ----
     overall_stats <- reactive({
       req(all_tasks())
       get_overall_stats(all_tasks())
     })
-    
     client_stats <- reactive({
       req(all_tasks())
       get_client_stats(all_tasks())
     })
-    
     author_stats <- reactive({
       req(all_tasks())
       get_author_stats(all_tasks())
     })
-    
     proj_elements_stat <- reactive({
       req(all_tasks())
       get_proj_elements_stat(all_tasks())
     })
     
-    # Вывод общей статистики
+    # ---- Новый реактив для таблицы сбоев ----
+    task_errors <- reactive({
+      req(input$error_date_range)
+      
+      dbReadTable(con, "task_log") %>%
+        mutate(log_time = str_sub(log_time, 1, 10) %>% as.Date()) %>%
+        filter(log_time >= input$error_date_range[1],
+               log_time <= input$error_date_range[2]) %>%
+        summarise(errors = n_distinct(log_time), .by = task_name) %>%
+        arrange(desc(errors))
+    })
+    
+    output$task_errors_table <- renderDT({
+      datatable(task_errors(),
+                options = list(pageLength = 10, scrollX = TRUE),
+                selection = "none")
+    })
+    
+    # ---- Существующие UI-рендеры ----
     output$overall_stats_summary <- renderUI({
       stats <- overall_stats()
       HTML(paste0(
@@ -113,22 +144,19 @@ mod_tab_statistic_server <- function(id, all_tasks, conf_rv) {
       ))
     })
     
-    # Рендеринг таблиц статистики
     output$client_stats_table <- renderDT({
       datatable(client_stats(), options = list(pageLength = 10, scrollX = TRUE), selection = 'none')
     })
-    
     output$author_stats_table <- renderDT({
       datatable(author_stats(), options = list(pageLength = 10, scrollX = TRUE), selection = 'none')
     })
-    
     output$proj_elements_stats_table <- renderDT({
       datatable(proj_elements_stat(), options = list(pageLength = 10, scrollX = TRUE), selection = 'none')
     })
     
+    # ---- Графики ----
     # График наличия элементов проекта
     output$proj_elements_plot <- renderPlot(({
-      
       indicators <- as.data.frame(overall_stats()) %>% 
         select(matches('rate|new_structure_percent')) %>% 
         pivot_longer(everything()) %>% 
@@ -147,20 +175,16 @@ mod_tab_statistic_server <- function(id, all_tasks, conf_rv) {
         )
       
       ggplot(indicators, aes(y = ypos)) +
-        # фоновые зоны (например, красный-жёлтый-зелёный)
         geom_rect(aes(xmin = 0, xmax = 0.5, ymin = ypos - 0.2, ymax = ypos + 0.2),
                   fill = "#ffcccc") +
         geom_rect(aes(xmin = 0.5, xmax = 0.8, ymin = ypos - 0.2, ymax = ypos + 0.2),
                   fill = "#fff2cc") +
         geom_rect(aes(xmin = 0.8, xmax = 1.0, ymin = ypos - 0.2, ymax = ypos + 0.2),
                   fill = "#d9ead3") +
-        # фактическое значение (тёмная полоса)
         geom_rect(aes(xmin = 0, xmax = value, ymin = ypos - 0.1, ymax = ypos + 0.1),
                   fill = "steelblue") +
-        # цель (вертикальная линия)
         geom_segment(aes(x = target, xend = target, y = ypos - 0.25, yend = ypos + 0.25),
                      color = "black", size = 1) +
-        # подпись справа
         geom_text(aes(x = 1.05, label = scales::percent(value, accuracy = 1)),
                   hjust = 0, size = 4, color = "grey15") +
         scale_x_continuous(labels = scales::percent, limits = c(0, 1.1)) +
@@ -173,13 +197,8 @@ mod_tab_statistic_server <- function(id, all_tasks, conf_rv) {
           axis.text.y = element_text(size = 16)
         ) + 
         labs(title = 'Наличие элементов проекта у скриптов')
-      
     }))
     
-    # Подключение к базе данных для графиков
-    con <- dbConnect(SQLite(), conf_rv()$database_settings$task_log_base)
-    
-    # График динамики сбоев
     output$task_log_plot <- renderPlot({
       dbReadTable(con, "task_log") %>% 
         mutate(log_time = str_sub(log_time, 1, 10) %>% as.Date()) %>% 
@@ -190,7 +209,6 @@ mod_tab_statistic_server <- function(id, all_tasks, conf_rv) {
         labs(title = 'Динамика сбоев планировщика заданий', x = '', y = '')
     })
     
-    # График количества заданий
     output$task_info_plot <- renderPlot({
       dbReadTable(con, "task_info_daily") %>% 
         mutate(log_time = str_sub(log_time, 1, 10) %>% as.Date()) %>% 
@@ -201,6 +219,5 @@ mod_tab_statistic_server <- function(id, all_tasks, conf_rv) {
         geom_line() + geom_point() + 
         labs(title = 'Динамика количества настроенных заданий в планировщике', x = '', y = '')
     })
-    
   })
 }
